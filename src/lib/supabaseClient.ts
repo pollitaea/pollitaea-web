5a3b2bc5ac1d60256be2be732800d90bc102048c
Samuel Adetunji <guysnacho2@gmail.com>
2023-10-08 03:53:24 -0500

    Add supabase types

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,4 +1,5 @@
 import { SUPABASE_URL, SUPABASE_ANON_KEY } from '$env/static/private';
 import { createClient } from '@supabase/supabase-js';
+import type { Database } from './schema';
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
