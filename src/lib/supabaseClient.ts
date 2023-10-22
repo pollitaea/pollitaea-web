669515d997ac119913751be029548c7416ac125a
guysnacho <guysnacho2@gmail.com>
2023-10-21 20:14:39 -0500

    Dont persist session

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -2,4 +2,8 @@ import { SUPABASE_URL, SUPABASE_ANON_KEY } from '$env/static/private';
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './schema';
 
-export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY, {
+    auth: {
+        persistSession: false,
+    }
+});
