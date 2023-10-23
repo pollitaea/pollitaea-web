502ad6e8804f01f3fb88ba6dd55c75507e83cbc1
guysnacho <guysnacho2@gmail.com>
2023-10-22 22:49:12 -0500

    Add representative search

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,3 +5,8 @@ export const HttpCodes = {
 	OH_NAH: 403,
 	INTERNALERROR: 500
 };
+
+export const ExternalServices = {
+	RepSearch: "https://www.googleapis.com/civicinfo/v2/representatives",
+	VoterInfo: "https://www.googleapis.com/civicinfo/v2/voterinfo",
+}
--- src/routes/api/representative/+server.ts
+++ src/routes/api/representative/+server.ts
@@ -1,10 +1,31 @@
 import { isValidPolRequest } from '$lib/common';
-import { json, type RequestHandler } from '@sveltejs/kit';
+import querystring from 'node:querystring';
+import { error, json, type RequestHandler } from '@sveltejs/kit';
+import { REPRESENTATIVE_KEY } from '$env/static/private';
+import { ExternalServices } from '$lib/constants';
 
-export const GET = (async ({request}) => {
-	console.debug(request.headers.get("id"));
-	
-	await isValidPolRequest(request)
+export const GET = (async ({ request }) => {
+	const requestQuery = querystring.parse(request.url.split('?')[1]).address;
+	await isValidPolRequest(request, requestQuery);
+	console.log(
+		'Request\n' +
+			ExternalServices.RepSearch +
+			'?' +
+			querystring.stringify({ key: REPRESENTATIVE_KEY, address: requestQuery })
+	);
 
-	return json("Successful request")
+	return await fetch(
+		ExternalServices.RepSearch +
+			'?' +
+			querystring.stringify({ key: REPRESENTATIVE_KEY, address: requestQuery })
+	)
+		.then(async (response) => {
+			const data = await response.json();
+			console.log(data);
+			return json(data);
+		})
+		.catch((err) => {
+			console.log(err);
+			throw error(err.status, { code: err.status, message: err.statusText });
+		});
 }) satisfies RequestHandler;
