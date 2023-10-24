8905b2d5e7bb20f4d99c10061be738c82bd1a37f
guysnacho <guysnacho2@gmail.com>
2023-10-23 20:45:02 -0500

    Add voter info types

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -54,3 +54,62 @@ export type CivicInfoResponse = {
 	offices: Office[];
 	officials: Official[];
 };
+
+type Election = {
+	id: string;
+	name: string;
+	electionDay: string;
+	ocdDivisionId: string;
+};
+
+type PollingLocation = {
+	address: {
+		locationName: string;
+		line1: string;
+		city: string;
+		state: string;
+		zip: string;
+	};
+	pollingHours: string;
+	startDate: string;
+	endDate: string;
+	sources: [
+		{
+			name: string;
+			official: boolean;
+		}
+	];
+};
+
+type StateElectionInfo = {
+	name: string;
+	electionAdministrationBody: {
+		name: string;
+		electionInfoUrl: string;
+		electionRegistrationUrl: string;
+		electionRegistrationConfirmationUrl: string;
+		absenteeVotingInfoUrl: string;
+		ballotInfoUrl: string;
+		physicalAddress: {
+			locationName: string;
+			line1: string;
+			city: string;
+			state: string;
+			zip: string;
+		};
+	};
+	sources: [
+		{
+			name: string;
+			official: boolean;
+		}
+	];
+};
+
+export type VoterInfoResponse = {
+	election: Election;
+	normalizedInput: Address;
+	pollingLocations: PollingLocation[];
+	earlyVoteSites: PollingLocation[];
+	state: StateElectionInfo[];
+};
